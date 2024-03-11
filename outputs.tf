3302c5208b24bff5fff4de857a48a579bd3f84fa
Benjamin Sjølander <17160920+putetrekk@users.noreply.github.com>
2024-03-11 11:31:04 +0100

    🚑🔥 Remove deprecated lambda_permission output. (#12)
    PR #11 did not remove the final reference to the bucket invoke lambda permission.
    
    This hotfix removes said output to avoid referencing a non-existent resource declaration.

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,3 @@ output "invoke_arn" {
 output "function_name" {
   value = aws_lambda_function.lambda.function_name
 }
-output "lambda_permission" {
-  value = var.allow_bucket != "" ? aws_lambda_permission.allow_bucket[0].id : ""
-}
